22fda3702ab421c741d5d4e316c47185c857f21b
blueishot <fabricioteixeirahungria@gmail.com>
2022-04-05 23:36:11 -0300

    media query

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+// let encounters = window.document.getElementsByClassName('teste')[0]
+
+// function menu(x) {
+//     if (x.matches) {
+//         encounters.value.innerHTML = 'Não sou mais encounters'
+//     } else {
+//         encounters.value.innerHTML = 'es'
+//     }
+// }
+
+
+// let x = window.matchMedia("(max-width: 1172px)")
+// menu(x)
+// x.addEventListener(menu(x))
+
