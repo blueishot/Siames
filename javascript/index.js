3eb10730f4acf941bdea0317b2078f65f016d378
blueishot <fabricioteixeirahungria@gmail.com>
2022-04-10 01:21:37 -0300

    album done

--- javascript/index.js
+++ javascript/index.js
@@ -25,3 +25,12 @@ class MobileMenu {
 
 const mobile = new MobileMenu('.mobileMenu', "buttonsMenu", '.buttonsMenu div')
 mobile.init()
+
+
+function linkToHome() {
+    window.location = "https://siames.ochre.store/release/206182-siames-home"
+}
+
+function linkToBounce() {
+    window.location = "https://siames.ochre.store/release/206139-siames-bounce-into-the-music"
+}
